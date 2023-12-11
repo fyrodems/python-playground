ceece583f6295fac69482502fd2a4cb1d9e932ac
fyrodems <fyrodems@gmail.com>
2023-12-11 10:40:08 +0100

    add config to generateGarbageSeed.py

--- śmietniczki/generateGarbageSeed.py
+++ śmietniczki/generateGarbageSeed.py
@@ -1,6 +1,14 @@
 import random
 from datetime import datetime, timedelta
 
+# Stała konfiguracyjna
+CONFIG = {
+    'user_ids': [4, 14, 15, 8],
+    'dumpster_id_1': 4,
+    'dumpster_id_2': 6,
+    'entries_per_user': 10,
+}
+
 # Funkcja do generowania losowej daty w zakresie od 1 stycznia 2022 do 31 grudnia 2024
 def generate_random_date():
     start_date = datetime(2022, 1, 1)
@@ -27,19 +35,27 @@ def save_to_file(data, file_name):
             file.write(f"  {entry},\n")
         file.write("]\n")
 
+        # Dodaj strukturę createList na koniec pliku
+        file.write("\nconst createList = (list) => {\n")
+        file.write("  return list.map((g) => ({\n")
+        file.write("    garbage_ID: g[0],\n")
+        file.write("    garbage_usersID: g[1],\n")
+        file.write("    garbage_dumpsterID: g[2],\n")
+        file.write("    garbage_typeID: g[3],\n")
+        file.write("    garbage_weight: g[4],\n")
+        file.write("    garbage_date: new Date(g[5]),\n")
+        file.write("  }))\n")
+        file.write("\nexport default createList(garbageData)")
+
 # Main
 file_name = 'garbage.js'
 
-# Generuj 10 rekordów dla użytkowników o id 4, 14, 15 (z id śmietnika równym 4)
-garbage_data_1 = generate_garbage_data(4, 4, 10)
-garbage_data_2 = generate_garbage_data(14, 4, 10)
-garbage_data_3 = generate_garbage_data(15, 4, 10)
-
-# Generuj 10 rekordów dla użytkowników o id 8 (z id śmietnika równym 6)
-garbage_data_4 = generate_garbage_data(8, 6, 10)
-
-# Połącz dane dla wszystkich użytkowników
-all_garbage_data = garbage_data_1 + garbage_data_2 + garbage_data_3 + garbage_data_4
+# Generuj dane dla użytkowników zgodnie z konfiguracją
+all_garbage_data = []
+for user_id in CONFIG['user_ids']:
+    bin_id = CONFIG['dumpster_id_1'] if user_id != 8 else CONFIG['dumpster_id_2']
+    user_data = generate_garbage_data(user_id, bin_id, CONFIG['entries_per_user'])
+    all_garbage_data.extend(user_data)
 
 # Zapisz dane do pliku
 save_to_file(all_garbage_data, file_name)
